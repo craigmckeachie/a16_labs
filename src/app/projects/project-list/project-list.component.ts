7710862c7cf2e6c9e6e20db14d2e11d2afa40f29
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 13:09:02 -0400

    lab06

---
+++ src/app/projects/project-list/project-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Project } from '../shared/project.model';
+
+@Component({
+  selector: 'app-project-list',
+  templateUrl: './project-list.component.html',
+  styleUrls: ['./project-list.component.css'],
+})
+export class ProjectListComponent implements OnInit {
+  @Input()
+  projects: Project[] = [];
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/projects/projects.module.ts
+++ src/app/projects/projects.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsContainerComponent } from './projects-container/projects-container.component';
+import { ProjectListComponent } from './project-list/project-list.component';
 
 @NgModule({
-  declarations: [ProjectsContainerComponent],
+  declarations: [ProjectsContainerComponent, ProjectListComponent],
   imports: [CommonModule, ProjectsRoutingModule],
   exports: [ProjectsContainerComponent],
 })
