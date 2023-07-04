b77d0c8be78005348208d85b38369345e2f09ca3
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 13:09:00 -0400

    lab04

---
+++ src/app/projects/projects-container/projects-container.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProjectsContainerComponent } from './projects-container.component';
+
+describe('ProjectsContainerComponent', () => {
+  let component: ProjectsContainerComponent;
+  let fixture: ComponentFixture<ProjectsContainerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProjectsContainerComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ProjectsContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/projects/projects-container/projects-container.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-container',
+  templateUrl: './projects-container.component.html',
+  styleUrls: ['./projects-container.component.css']
+})
+export class ProjectsContainerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
