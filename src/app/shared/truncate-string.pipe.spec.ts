930e7ae9b1c8f89f59e540f384f4ff66558f1db2
Craig McKeachie <craigdmckeachie@gmail.com>
2023-07-04 13:32:44 -0400

    unit-lab06

--- src/app/shared/truncate-string.pipe.spec.ts
+++ src/app/shared/truncate-string.pipe.spec.ts
@@ -1,8 +1,16 @@
 import { TruncateStringPipe } from './truncate-string.pipe';
 
 describe('TruncateStringPipe', () => {
+  const pipe = new TruncateStringPipe();
   it('create an instance', () => {
-    const pipe = new TruncateStringPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('truncates string at given length', () => {
+    expect(pipe.transform('abc', 2)).toEqual('ab...');
+  });
+
+  it('returns same string if length equals the length', () => {
+    expect(pipe.transform('ab', 2)).toEqual('ab');
   });
 });
